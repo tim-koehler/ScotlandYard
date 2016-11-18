9d24f715fca6e13254f7e781daceaf761af3b160
Marko Boger <marko.boger@gmail.com>
2016-11-18 08:56:17 +0100

    Update StudentSpec.scala

--- src/test/scala/de/htwg/se/yourgame/model/StudentSpec.scala
+++ src/test/scala/de/htwg/se/yourgame/model/StudentSpec.scala
@@ -1,7 +1,10 @@
 package de.htwg.se.yourgame.model
 
 import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner]
 class StudentSpec extends FlatSpec with Matchers {
   "A Student" should "have a name" in {
     Student("Your Name").name should be("Your Name")
