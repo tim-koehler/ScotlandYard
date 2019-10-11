1bf94b5ab27dff3214109ec960950f4c2e1b0812
RaZorfalkon <timkoehler97@live.de>
2019-10-11 11:02:06 +0200

    Updated class name

--- src/main/scala/de/htwg/se/scotlandyard/ScotlandYard.scala
+++ src/main/scala/de/htwg/se/scotlandyard/ScotlandYard.scala
@@ -2,7 +2,7 @@ package de.htwg.se.scotlandyard
 
 import de.htwg.se.scotlandyard.model.Player
 
-object YourGame {
+object ScotlandYard {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
