a363a358015c26572fb35e4a57bc0d2c34a5a873
Roland Burke <Roland.Burke@gmx.de>
2019-10-11 12:50:24 +0200

    changed message

--- src/main/scala/de/htwg/se/scotlandyard/ScotlandYard.scala
+++ src/main/scala/de/htwg/se/scotlandyard/ScotlandYard.scala
@@ -7,7 +7,7 @@ object ScotlandYard {
   def main(args: Array[String]): Unit = {
     val player1 = Player("Tim")
     val player2 = Player("Roli")
-    println("Hello, " + player1.name)
-    println("Hello, " + player2.name)
+    println("Hello, " + player1.name + "How are you doing?")
+    println("Hello, " + player2.name + "How are you doing?")
   }
 }
