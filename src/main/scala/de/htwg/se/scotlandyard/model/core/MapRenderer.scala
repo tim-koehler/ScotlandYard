70b8c861928d7b366ba483a35c0d6b352a61c6bd
tim-koehler <timkoehler97@live.de>
2019-11-07 14:56:04 +0100

    Improved Map rendering

--- src/main/scala/de/htwg/se/scotlandyard/model/core/MapRenderer.scala
+++ src/main/scala/de/htwg/se/scotlandyard/model/core/MapRenderer.scala
@@ -8,6 +8,11 @@ object MapRenderer {
   var offsetX: Int = 0
   var offsetY: Int = 0
 
+  val renderDimensionX = 200;
+  val renderDimensionY = 33;
+
+  val mapMoveOffset = 5;
+
   def init() : Boolean = {
     val source = Source.fromFile("./src/main/scala/de/htwg/se/scotlandyard/ScotlandYardMap.txt")
     for (line <- source.getLines()) {
@@ -23,33 +28,60 @@ object MapRenderer {
     true
   }
 
-  def updateX(newX: Integer): Int ={
-    offsetX += newX
-    if(offsetX < 0) {
-      offsetX = 0
-    };
-    offsetX
+  def updateX(moveMultiplicator: Int, positive: Boolean ): Int = {
+    if(positive) {
+      offsetX += moveMultiplicator * mapMoveOffset;
+    }
+    else{
+      offsetX -= moveMultiplicator * mapMoveOffset;
+    }
+    keepInBoundsX()
   }
 
-  def updateY(newY: Integer): Int ={
-    offsetY += newY
+  def keepInBoundsX(): Int = {
+    if(offsetX < 0) {
+      offsetX = 0
+    }
+    else if(offsetX + renderDimensionX > 280) {
+      offsetX = 280 - renderDimensionX;
+    }
+    offsetX;
+  }
+
+  def updateY(moveMultiplicator: Int, positive: Boolean ): Int = {
+    if(positive) {
+      offsetX += moveMultiplicator * mapMoveOffset;
+    }
+    else{
+      offsetX -= moveMultiplicator * mapMoveOffset;
+    }
+    keepInBoundsY()
+  }
+
+  def keepInBoundsY(): Int = {
     if(offsetY < 0) {
       offsetY = 0
-    };
+    }
     offsetY
   }
 
   def renderMap(): String = {
       var str = ""
-      for (i <- offsetY until (35 + offsetY); j <- offsetX until (300 + offsetX))
+      for (y <- offsetY until (renderDimensionY + offsetY))
       {
-        try {
-          str += Map.map(i).charAt(j)
+        for(x <- offsetX until (renderDimensionX + offsetX))
+        {
+          try {
+            str += Map.map(y).charAt(x)
+          }
+          catch  {
+            case e: Exception => str += " "
+          }
         }
-        catch  {
-          case e: Exception => str += " "
-        }
+        str += "\n"
       }
+      println("OffsetX: " + offsetX)
+      println("OffsetY: " + offsetY)
       str + "\n"
     }
 }
