4825808a15114dc52359ff5c095a3e6a69f74776
tim-koehler <timkoehler97@live.de>
2019-11-08 12:54:29 +0100

    Some work

--- src/main/scala/de/htwg/se/scotlandyard/model/map/Map.scala
+++ src/main/scala/de/htwg/se/scotlandyard/model/map/Map.scala
@@ -1,13 +1,20 @@
 package de.htwg.se.scotlandyard.model.map
 
 import de.htwg.se.scotlandyard.model.core.MapRenderer
+import de.htwg.se.scotlandyard.model.player.Player
 
 object Map {
   var map: List[String] = List()
-  val stations: Set[Station] = Set()
-  var playerPositions = List()
+  val stations: List[Station] = List()
+  var playerPositions: Map[Player, Int] = scala.collection.immutable.Map()
 
+  //TODO: CLEANUP!!
+  def initStations(): Int = {
 
+    stations = new Station(1) :: stations
+    stations(1)
+    1
+  }
 
   override def toString: String = {
     MapRenderer.renderMap()
