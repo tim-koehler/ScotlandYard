d5d0bd69b09bcd226d43db0f38501db2d2c9d4b1
Roland Burke <Roland.Burke@gmx.de>
2020-01-14 17:58:17 +0100

    you can save now to xml, load from xml not implemented yet

--- src/main/scala/de/htwg/se/scotlandyard/controllerComponent/controllerBaseImpl/Controller.scala
+++ src/main/scala/de/htwg/se/scotlandyard/controllerComponent/controllerBaseImpl/Controller.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.scotlandyard.controllerComponent.controllerBaseImpl
 
 import de.htwg.se.scotlandyard.controllerComponent.{ControllerInterface, NumberOfPlayersChanged, PlayerMoved, PlayerNameChanged, PlayerWin, StartGame}
-import de.htwg.se.scotlandyard.model.core.fileIoComponent.fileIoJsonImpl.FileIO
+import de.htwg.se.scotlandyard.model.core.fileIoComponent.fileIOXmlImpl.FileIO
 import de.htwg.se.scotlandyard.model.core.{GameInitializer, GameMaster}
 import de.htwg.se.scotlandyard.model.map.station.Station
 import de.htwg.se.scotlandyard.model.playersComponent.{DetectiveInterface, MrXInterface}
--- src/main/scala/de/htwg/se/scotlandyard/model/core/fileIoComponent/fileIOXmlImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/scotlandyard/model/core/fileIoComponent/fileIOXmlImpl/FileIO.scala
@@ -1,17 +1,80 @@
 package de.htwg.se.scotlandyard.model.core.fileIoComponent.fileIOXmlImpl
 
+import java.awt.Color
+import java.io._
+
 import de.htwg.se.scotlandyard.model.core.GameMaster
 import de.htwg.se.scotlandyard.model.core.fileIoComponent.FileIOInterface
-import de.htwg.se.scotlandyard.model.playersComponent.playersBaseImpl.MrX
+
+import scala.swing.Color
+import scala.xml._
 
 class FileIO extends FileIOInterface {
+
   override def load(): Unit = {
 
   }
 
   override def save(): Unit = {
-    val players = GameMaster.players
-    
+    val pw = new PrintWriter(new File("ScotlandYard.xml"))
+    val prettyPrinter = new PrettyPrinter(120, 4)
+    val xml = prettyPrinter.format(gametoXML())
+    pw.write(xml)
+    pw.close
+  }
+
+  def gametoXML(): Elem = {
+    <game>
+      <round>{GameMaster.round}</round>
+      <totalRound>{GameMaster.totalRound}</totalRound>
+      <nPlayer>{GameMaster.players.length}</nPlayer>
+      {mrXtoXml()}
+      {allDetectivesToXml()}
+    </game>
+  }
+
+  def mrXHistoryToXml(): Elem = {
+    var xmlString: String = ""
+    if(GameMaster.getMrX().history.isEmpty) {
+      xmlString = "<transport></transport>"
+    }
+    for(h <- GameMaster.getMrX().history) {
+      xmlString = "<transport>" + h + "</transport>" + xmlString
+    }
+    XML.loadString(xmlString)
+  }
+
+  def mrXtoXml(): Elem = {
+    <mrX>
+      <name>{GameMaster.getMrX().name}</name>
+      <stationNumber>{GameMaster.getMrX().station.number}</stationNumber>
+      <isVisible>{GameMaster.getMrX().isVisible}</isVisible>
+      <lastSeen>{GameMaster.getMrX().lastSeen}</lastSeen>
+      <blackTickets>{GameMaster.getMrX().blackTickets}</blackTickets>
+      <doubleTurns>{GameMaster.getMrX().doubleTurn}</doubleTurns>
+      <taxiTickets>{GameMaster.getMrX().taxiTickets}</taxiTickets>
+      <busTickets>{GameMaster.getMrX().busTickets}</busTickets>
+      <undergroundTickets>{GameMaster.getMrX().undergroundTickets}</undergroundTickets>
+      <history>{mrXHistoryToXml}</history>
+    </mrX>
+  }
+
+  def allDetectivesToXml(): Elem = {
+    var detectiveString = ""
+    for(i <- 1 to GameMaster.players.length - 1) {
+      detectiveString = detectiveString + detectiveToXmlString(GameMaster.players(i).name, GameMaster.players(i).station.number, GameMaster.players(i).taxiTickets, GameMaster.players(i).busTickets, GameMaster.players(i).undergroundTickets, GameMaster.players(i).color)
+    }
+    detectiveString = "<detectives>" + detectiveString + "</detectives>"
+    XML.loadString(detectiveString)
+  }
+
+  def detectiveToXmlString(name: String, stationNumber: Int, taxiTickets: Int, busTickets: Int, undergroundTickets: Int, color: Color): String = {
+    "<detective><name>{" + name + "}</name> " +
+      "<stationNumber>{" + stationNumber + "}</stationNumber>" +
+      "<taxiTickets>{" + taxiTickets + "}</taxiTickets>" +
+      "<busTickets>{" + busTickets + "}</busTickets>" +
+      "<undergroundTickets>{" + undergroundTickets + "}</undergroundTickets>" +
+      "<color>{" + String.valueOf(color.getRGB) + "}</color></detective>"
   }
 
 }
