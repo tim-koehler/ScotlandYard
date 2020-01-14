52bcec52008d555a314f09d5b482416ba815f15d
Roland Burke <Roland.Burke@gmx.de>
2020-01-14 18:56:41 +0100

    mrX data can now be loaded from xml

--- src/main/scala/de/htwg/se/scotlandyard/model/core/fileIoComponent/fileIOXmlImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/scotlandyard/model/core/fileIoComponent/fileIOXmlImpl/FileIO.scala
@@ -3,8 +3,10 @@ package de.htwg.se.scotlandyard.model.core.fileIoComponent.fileIOXmlImpl
 import java.awt.Color
 import java.io._
 
-import de.htwg.se.scotlandyard.model.core.GameMaster
+import de.htwg.se.scotlandyard.model.core.{GameInitializer, GameMaster}
 import de.htwg.se.scotlandyard.model.core.fileIoComponent.FileIOInterface
+import de.htwg.se.scotlandyard.util.TicketType
+import de.htwg.se.scotlandyard.util.TicketType.TicketType
 
 import scala.swing.Color
 import scala.xml._
@@ -12,6 +14,27 @@ import scala.xml._
 class FileIO extends FileIOInterface {
 
   override def load(): Unit = {
+    val xmlFile = scala.xml.XML.loadFile("ScotlandYard.xml")
+    GameMaster.round = (xmlFile \\ "game" \ "round").text.toInt
+    GameMaster.totalRound = (xmlFile \\ "game" \ "totalRound").text.toInt
+    val name = (xmlFile \\ "game" \ "mrX" \ "name").text
+    val stationNumber = (xmlFile \\ "game" \ "mrX" \ "stationNumber").text.toInt
+    val isVisible = (xmlFile \\ "game" \ "mrX" \ "isVisible").text.toBoolean
+    val lastSeen = (xmlFile \\ "game" \ "mrX" \ "lastSeen").text
+    val blackTickets = (xmlFile \\ "game" \ "mrX" \ "blackTickets").text.toInt
+    val doubleTurns = (xmlFile \\ "game" \ "mrX" \ "doubleTurns").text.toInt
+    val taxiTickets = (xmlFile \\ "game" \ "mrX" \ "taxiTickets").text.toInt
+    val busTickets = (xmlFile \\ "game" \ "mrX" \ "busTickets").text.toInt
+    val undergroundTickets = (xmlFile \\ "game" \ "mrX" \ "undergroundTickets").text.toInt
+    var history: List[TicketType] = List()
+    for(i <- 0 to (xmlFile \\ "game" \ "mrX" \ "history").length - 1) {
+      var s: String = (xmlFile \\ "game" \ "mrX" \ "history")(i).text
+      s = s.trim.replaceAll("\n ", "")
+      history = history:::List(TicketType.withName(s))
+    }
+    GameInitializer.initMrXFromLoad(name, stationNumber, isVisible, lastSeen, blackTickets, doubleTurns, taxiTickets, busTickets, undergroundTickets, history)
+
+
 
   }
 
