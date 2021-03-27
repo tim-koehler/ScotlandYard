19c4e085146bee4fc0aefb226542a69e8d4477ab
Roland Burke <Roland.Burke@gmx.de>
2021-03-27 16:12:17 +0100

    started with GameModel Tests

--- src/test/scala/de/htwg/se/scotlandyard/model/coreComponent/GameModelSpec.scala
+++ src/test/scala/de/htwg/se/scotlandyard/model/coreComponent/GameModelSpec.scala
@@ -1,6 +1,5 @@
 package de.htwg.se.scotlandyard.model.coreComponent
 
-import de.htwg.se.scotlandyard.model.{TicketType}
 import de.htwg.se.scotlandyard.controller.gameInitializerComponent.gameInitializerMockImpl.GameInitializer
 import de.htwg.se.scotlandyard.model.playersComponent.playersMockImpl.MrX
 import org.scalatest._
@@ -8,46 +7,45 @@ import org.scalatest._
 class gameModelSpec extends WordSpec with Matchers with PrivateMethodTester {
   "GameMaster Object" when {
     val gameInitializer = new GameInitializer
-    val gameModel = gameInitializer.initialize(2)
-    "initialize() is called should" should {
-      "return true" in {
-        gameInitializer.initialize(2).players.length should be (2)
-      }
-      "and the current player should be MrX" in {
-        gameModel.getCurrentPlayer.isInstanceOf[MrX]
-      }
-      "and the current player Index should be 0" in {
-        //gameModel.round = 1
-        gameModel.getCurrentPlayerIndex shouldBe 0
-      }
-      "tickets counts" in {
-        val currentTaxiTickets = gameModel.getCurrentPlayer.tickets.taxiTickets
-        gameModel.updateTickets(TicketType.Taxi)(gameModel.incrementValue) should be(currentTaxiTickets + 1)
-
-        val currentBusTickets = gameModel.getCurrentPlayer.tickets.busTickets
-        gameModel.updateTickets(TicketType.Bus)(gameModel.incrementValue) should be(currentBusTickets + 1)
-
-        val currentUndergroundTickets = gameModel.getCurrentPlayer.tickets.undergroundTickets
-        gameModel.updateTickets(TicketType.Underground)(gameModel.incrementValue) should be(currentUndergroundTickets + 1)
-
-        val currentBlackTickets = gameModel.getMrX.tickets.blackTickets
-        gameModel.updateTickets(TicketType.Black)(gameModel.incrementValue) should be(currentBlackTickets + 1)
-
-        gameModel.updateTickets(TicketType.Taxi)(gameModel.decrementValue) should be(currentTaxiTickets)
-        gameModel.updateTickets(TicketType.Bus)(gameModel.decrementValue) should be(currentBusTickets)
-        gameModel.updateTickets(TicketType.Underground)(gameModel.decrementValue) should be(currentUndergroundTickets)
-        gameModel.updateTickets(TicketType.Black)(gameModel.decrementValue) should be(currentBlackTickets)
+    val gameModel = gameInitializer.initialize(3)
+    "getCurrentPlayer is called should" should {
+      "return MrX" in {
+        gameModel.getCurrentPlayer(gameModel.players, 1).isInstanceOf[MrX]
       }
     }
-    "getLastPlayer() is called" should {
-      gameInitializer.initialize(2)
-      "return the last detective if Mrx is current Player" in {
-        //gameModel.round = 2
-        gameModel.getPreviousPlayer.name should be("MrX")
+    "getPreviousPlayer is called should" should {
+      "return Last Detective" in {
+        gameModel.getCurrentPlayer(gameModel.players, 1).name should be("Dt2")
       }
-      "return the last player if a detective is the current player" in {
-        //gameModel.round = 1
-        gameModel.getPreviousPlayer.name should be("Dt1")
+      "return MrX" in {
+        gameModel.getCurrentPlayer(gameModel.players, 2).isInstanceOf[MrX] should be(true)
+      }
+    }
+    "getMrX is called should" should {
+      "return MrX" in {
+        gameModel.getMrX(gameModel.players).isInstanceOf[MrX] should be (true)
+      }
+    }
+    "getDetectives is called should" should {
+      "return all detectives" in {
+        gameModel.getDetectives(gameModel.players).length should be(2)
+        gameModel.getDetectives(gameModel.players)(0).name should be("Dt1")
+      }
+    }
+    "getCurrentPlayer is called should" should {
+      "return 0 in round 1" in {
+        gameModel.getCurrentPlayerIndex(gameModel.players, 1) should be (0)
+      }
+      "return also 0 in round 4" in {
+        gameModel.getCurrentPlayerIndex(gameModel.players, 1) should be (0)
+      }
+      "return 1 in round 2" in {
+        gameModel.getCurrentPlayerIndex(gameModel.players, 2) should be (1)
+      }
+    }
+    "updatePlayerPosition is called should" should {
+      "return the new Station" in {
+        gameModel.updatePlayerPosition(gameModel.getCurrentPlayer(gameModel.players, 1), 5).number should be (5)
       }
     }
   }
