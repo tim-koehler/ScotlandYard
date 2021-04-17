7d53635fb4c8204e5be4a03b4853486c8a82687d
tim-koehler <timkoehler97@live.de>
2021-04-17 18:09:11 +0200

    asd

--- Model/src/main/scala/de/htwg/se/scotlandyard/model/Rest.scala
+++ Model/src/main/scala/de/htwg/se/scotlandyard/model/Rest.scala
@@ -11,7 +11,7 @@ import scala.concurrent.Future
 import scala.io.StdIn
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import de.htwg.se.scotlandyard.model.players.{Detective, MrX}
-import spray.json.{DefaultJsonProtocol, JsonFormat, RootJsonFormat}
+import spray.json.{DefaultJsonProtocol, DeserializationException, JsArray, JsNumber, JsObject, JsString, JsValue, JsonFormat, RootJsonFormat}
 
 object ScotlandYardJsonProtocol extends DefaultJsonProtocol {
   implicit val ticketsFormat = jsonFormat4(Tickets)
@@ -55,5 +55,63 @@ object Rest {
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
+
+object ScotlandYardJsonProtocol extends DefaultJsonProtocol {
+  implicit object TicketsJsonFormat extends RootJsonFormat[Tickets] {
+    def write(tickets: Tickets): JsObject = JsObject(
+      "taxiTickets" -> JsNumber(tickets.taxiTickets),
+      "busTickets" -> JsNumber(tickets.busTickets),
+      "undergroundTickets" -> JsNumber(tickets.undergroundTickets),
+      "blackTickets" -> JsNumber(tickets.blackTickets)
+    )
+    def read(value: JsValue): Tickets = {
+      value.asJsObject.getFields("taxiTickets", "busTickets", "undergroundTickets", "blackTickets") match {
+        case Seq(JsNumber(taxiTickets), JsNumber(busTickets), JsNumber(undergroundTickets), JsNumber(blackTickets)) =>
+          Tickets(taxiTickets.toInt, busTickets.toInt, undergroundTickets.toInt, blackTickets.toInt)
+        case _ => throw DeserializationException("Tickets expected")
+      }
+    }
+  }
+  implicit object StationJsonFormat extends RootJsonFormat[Station] {
+    def write(station: Station): JsObject = JsObject(
+      "number" -> JsNumber(station.number),
+      "stationType" -> JsString(station.stationType.toString),
+      "blackStation" -> JsString(station.blackStation.toString),
+      "neighbourTaxis" -> JsArray(station.neighbourTaxis.asInstanceOf[JsArray]),
+      "neighbourBuses" -> JsArray(station.neighbourBuses.asInstanceOf[JsArray]),
+      "neighbourUndergrounds" -> JsArray(station.neighbourUndergrounds.asInstanceOf[JsArray]),
+      "tuiCoordinates" -> JsObject(
+        "x" -> JsNumber(station.tuiCoordinates.x),
+        "y" -> JsNumber(station.tuiCoordinates.y)
+      ),
+      "guiCoordinates" -> JsObject(
+        "x" -> JsNumber(station.guiCoordinates.x),
+        "y" -> JsNumber(station.guiCoordinates.y)
+      ))
+    def read(value: JsValue): Station = {
+      value.asJsObject.getFields(
+        "number",
+        "stationType",
+        "blackStation",
+        "neighbourTaxis",
+        "neighbourBuses",
+        "neighbourUndergrounds",
+        "tuiCoordinates",
+        "guiCoordinates",
+      ) match {
+        case Seq(JsNumber(number),
+        JsString(stationType),
+        JsString(blackStation),
+        JsArray(neighbourTaxis),
+        JsArray(neighbourBuses),
+        JsArray(neighbourUndergrounds),
+        JsObject(tuiCoordinates),
+        JsObject(guiCoordinates)) =>
+          Station(number.toInt, stationType, blackStation, neighbourTaxis, neighbourBuses, neighbourUndergrounds, tuiCoordinates, guiCoordinates)
+        case _ => throw DeserializationException("Station expected")
+      }
+    }
   }
 }
