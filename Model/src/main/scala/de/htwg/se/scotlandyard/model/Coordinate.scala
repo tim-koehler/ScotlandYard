2eccc42721ed82119550a83001843008889d6e7d
tim-koehler <timkoehler97@live.de>
2021-04-20 00:23:28 +0200

    added distance function to Coordinate

--- Model/src/main/scala/de/htwg/se/scotlandyard/model/Coordinate.scala
+++ Model/src/main/scala/de/htwg/se/scotlandyard/model/Coordinate.scala
@@ -4,3 +4,10 @@ case class Coordinate(
                      x: Int = 1,
                      y: Int = 1
                      )
+{
+  def distance (px: Int, py: Int): Double = {
+    val ppx = px - this.x
+    val ppy = py - this.y
+    Math.sqrt (ppx * ppx + ppy * ppy)
+  }
+}
