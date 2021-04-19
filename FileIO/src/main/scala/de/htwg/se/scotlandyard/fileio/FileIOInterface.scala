d9411608d1be184ef0311d3e337763451c7c3aba
tim-koehler <timkoehler97@live.de>
2021-04-20 00:51:40 +0200

    sbt sucks so bad!

--- FileIO/src/main/scala/de/htwg/se/scotlandyard/fileio/FileIOInterface.scala
+++ FileIO/src/main/scala/de/htwg/se/scotlandyard/fileio/FileIOInterface.scala
@@ -1,7 +1,6 @@
 package de.htwg.se.scotlandyard.fileio
 
 import de.htwg.se.scotlandyard.model.GameModel
-import de.htwg.se.scotlandyard.model.players.MrX
 
 trait FileIOInterface {
   def load(stationsFileContent: String): GameModel
