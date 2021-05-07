06d9f138dc68ab3f5a4bd4ee98cacc7ac87c1580
tim-koehler <timkoehler97@live.de>
2021-05-07 22:31:39 +0200

    easy working version

--- Persistence/src/main/scala/de/htwg/se/scotlandyard/persistence/mongodb/MongoDB.scala
+++ Persistence/src/main/scala/de/htwg/se/scotlandyard/persistence/mongodb/MongoDB.scala
@@ -1,34 +1,25 @@
 package de.htwg.se.scotlandyard.persistence.mongodb
 
-import de.htwg.se.scotlandyard.model.players.{Detective, MrX, PlayerTypes, Player}
-import de.htwg.se.scotlandyard.model.{GameModel, Station, Coordinate, Tickets, TicketType, StationType}
+import de.htwg.se.scotlandyard.model.JsonProtocol.GameModelJsonFormat
+import de.htwg.se.scotlandyard.model.GameModel
 import de.htwg.se.scotlandyard.persistence.PersistenceInterface
-import org.bson.codecs.configuration.CodecRegistries.fromRegistries
-import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
-import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
-import org.bson.codecs.configuration.CodecRegistries
-import org.bson.codecs.configuration.CodecRegistries._
-import org.mongodb.scala.bson.codecs.Macros._
-
+import de.htwg.se.scotlandyard.persistence.mongodb.Helpers.GenericObservable
+import spray.json.enrichAny
+import spray.json._
+import org.mongodb.scala._
 
 class MongoDB extends PersistenceInterface{
+  val client: MongoClient = MongoClient("mongodb://root:scotty4life@mongodb")
+  val database: MongoDatabase = client.getDatabase("scotlandyard")
+  val collection: MongoCollection[Document] = database.getCollection("savegame")
 
-  private val customCodecs = fromProviders(classOf[GameModel], classOf[Tickets], classOf[Coordinate], classOf[Station], classOf[TicketType], classOf[StationType], classOf[PlayerTypes], classOf[Player], classOf[MrX], classOf[Detective])
-
-  private val codecRegistry = fromRegistries(customCodecs,
-    DEFAULT_CODEC_REGISTRY)
-
-  val uri: String = "mongodb://root:scotty4life@mongodb/test?retryWrites=true&w=majority"
-
-  val client: MongoClient = MongoClient(uri)
-  val database: MongoDatabase = client.getDatabase("scotlandyard").withCodecRegistry(codecRegistry)
-
-  val collection: MongoCollection[GameModel] = database.getCollection("scotty")
-
-  override def load(): GameModel = ???
+  override def load(): GameModel = {
+    val doc: Document = collection.find().results().head
+    doc.getString("savegame").parseJson.convertTo[GameModel]
+  }
 
   override def save(gameModel: GameModel): Boolean = {
-    collection.insertOne(gameModel)
+    collection.insertOne(Document("savegame"-> gameModel.toJson.prettyPrint)).results()
     true
   }
 
