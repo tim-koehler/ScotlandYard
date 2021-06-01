a2db0ef01dd7507a694d7293f9180aa4893f06a0
Roland Burke <Roland.Burke@gmx.de>
2021-06-01 16:17:26 +0200

    fileio

--- Persistence/src/main/scala/de/htwg/se/scotlandyard/persistence/fileio/fileIOMockImpl/FileIO.scala
+++ Persistence/src/main/scala/de/htwg/se/scotlandyard/persistence/fileio/fileIOMockImpl/FileIO.scala
@@ -4,12 +4,14 @@ import de.htwg.se.scotlandyard.model.{GameModel, PersistenceGameModel}
 import de.htwg.se.scotlandyard.model.players.MrX
 import de.htwg.se.scotlandyard.persistence.PersistenceInterface
 
+import scala.concurrent.Future
+
 class FileIO() extends PersistenceInterface{
-  override def load(): PersistenceGameModel = PersistenceGameModel()
+  override def load(): Future[PersistenceGameModel] = Future.successful(PersistenceGameModel())
 
-  override def save(persistenceGameModel: PersistenceGameModel): Boolean = true
+  override def save(persistenceGameModel: PersistenceGameModel): Future[Boolean] = Future.successful(true)
 
-  override def update(persistenceGameModel: PersistenceGameModel): Boolean = true
+  override def update(persistenceGameModel: PersistenceGameModel): Future[Boolean] = Future.successful(true)
 
-  override def delete(): Boolean = true
+  override def delete(): Future[Boolean] = Future.successful(true)
 }
