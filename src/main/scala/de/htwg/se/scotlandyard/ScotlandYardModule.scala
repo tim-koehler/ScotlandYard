ed2017ab87a33b2a399f234f4c531b69088b3cdc
Roland Burke <Roland.Burke@gmx.de>
2021-06-20 18:43:43 +0200

    fixed

--- src/main/scala/de/htwg/se/scotlandyard/ScotlandYardModule.scala
+++ src/main/scala/de/htwg/se/scotlandyard/ScotlandYardModule.scala
@@ -2,12 +2,12 @@ package de.htwg.se.scotlandyard
 
 import com.google.inject.AbstractModule
 import de.htwg.se.scotlandyard.controller.ControllerInterface
-import de.htwg.se.scotlandyard.controller.controllerBaseImpl.rest.restBaseImpl.Rest
+import de.htwg.se.scotlandyard.controller.controllerBaseImpl.rest.RestInterface
 import net.codingwell.scalaguice.ScalaModule
 
 class ScotlandYardModule extends AbstractModule with ScalaModule {
     override def configure(): Unit = {
       bind[ControllerInterface].to[controller.controllerBaseImpl.Controller]
-      bind[Rest].to[controller.controllerBaseImpl.rest.restBaseImpl.Rest]
+      bind[RestInterface].to[controller.controllerBaseImpl.rest.restBaseImpl.Rest]
     }
 }
